5501a7ae65dde4e6716d9041a687df94800b6d7b
BernCS <63181937+BernCS@users.noreply.github.com>
2020-06-09 16:55:41 +0500

    Add files via upload

---
+++ Exersise8_30/Exersise8_30/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace Exersise8_30
+{
+    public class Edge
+    {
+        public int First { get; set; }
+        public int Second { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите кол-во вершин: ");
+            int vertexCount = int.Parse(Console.ReadLine());
+            int[] vertexes = new int[vertexCount];
+            bool[] used = new bool[vertexCount];
+
+            Console.WriteLine($"Введите {vertexCount} вершин: ");
+            for (int i = 0; i < vertexCount; i++)
+            {
+                vertexes[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Введите кол-во ребер: ");
+            int edgesCount = int.Parse(Console.ReadLine());
+            Edge[] edges = new Edge[edgesCount];
+
+            for (int i = 0; i < edgesCount; i++)
+            {
+                edges[i] = new Edge();
+
+                Console.WriteLine($"Введите первую вершину {i + 1} ребра: ");
+                edges[i].First = int.Parse(Console.ReadLine());
+
+                Console.WriteLine($"Введите вторую вершину {i + 1} ребра: ");
+                edges[i].Second = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Введите K");
+            int k = int.Parse(Console.ReadLine());
+
+
+            int notEmptyVertexCount = 0;
+
+            for (int i = 0; i < edgesCount; i++)
+            {
+                for (int j = 0; j < vertexCount; j++)
+                {
+                    if (edges[i].First == vertexes[j] && !used[j])
+                    {
+                        used[j] = true;
+                        notEmptyVertexCount++;
+                    }
+                    if (edges[i].Second == vertexes[j] && !used[j])
+                    {
+                        used[j] = true;
+                        notEmptyVertexCount++;
+                    }
+                }
+            }
+
+            if (k <= vertexCount - notEmptyVertexCount)
+            {
+                for (int i = 0; i < vertexCount; i++)
+                {
+                    if (k == 0)
+                    {
+                        break;
+                    }
+                    if (!used[i])
+                    {
+                        Console.Write(vertexes[i] + " ");
+                        k--;
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Такого графа не существует");
+            }
+        }
+    }
+}
---
+++ Exersise9_22/Exersise9_22/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace Exersise9_22
+{
+    public class Node
+    {
+        int Data;
+        Node Next;
+        Node Previous;
+
+        public Node(int capacity, int index = 1)
+        {
+            if (capacity == 0)
+                return;
+
+            Data = index;
+            Next = new Node(capacity - 1, index + 1);
+            Next.Previous = this;
+
+            if (index == 1)
+                Previous = null;
+        }
+
+        public void Print()
+        {
+            if (Next == null)
+                return;
+            Console.Write(Data + " ");
+            Next.Print();
+        }
+
+        public void Contains(int data)
+        {
+            if (Next == null)
+            {
+                Console.WriteLine("Элемент не найден");
+                return;
+            }
+            if (Data == data)
+            {
+                Console.WriteLine("Элемент найден");
+                return;
+            }
+            Next.Contains(data);
+        }
+
+        public static void Remove(ref Node node, int data)
+        {
+            if (node.Data == data)
+            {
+                if (node.Previous == null)
+                {
+                    node = node.Next;
+                    node.Previous = null;
+                }
+                else
+                {
+                    node = node.Next;
+                }
+                Console.WriteLine("Элемент найден и удален");
+                return;
+            }
+            if (node.Next == null)
+            {
+                Console.WriteLine("Элемент не найден");
+                return;
+            }
+            Node.Remove(ref node.Next, data);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Node node = new Node(int.Parse(Console.ReadLine()));
+            node.Print();
+            Console.WriteLine();
+            node.Contains(10);
+            Node.Remove(ref node, 11);
+            node.Print();
+        }
+    }
+}
